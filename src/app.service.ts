b10b84feda18ceb72373ba2cd86838ba6385b08f
Akinori Nakata <nakata@real-its.com>
2024-01-15 10:09:19 +0900

    create configuration

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) { }
+
   getHello(): string {
+    Logger.debug(this.configService.get<string>('DATABASE_USER'), 'DATABASE_USER')
     return 'Hello World!';
   }
 }
