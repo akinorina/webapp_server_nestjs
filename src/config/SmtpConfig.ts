6d9a812193b5e5d252a3f60a0f2c03114a73720b
Akinori Nakata <nakata@real-its.com>
2024-01-17 19:54:23 +0900

    .env のBoolean値対応

--- src/config/SmtpConfig.ts
+++ src/config/SmtpConfig.ts
@@ -15,7 +15,7 @@ export function smtpConfig() {
     smtp: {
       host: process.env.SMTP_HOST,
       port: parseInt(process.env.SMTP_PORT, 10) || 25,
-      secure: process.env.SMTP_SECURE,
+      secure: process.env.SMTP_SECURE === 'true',
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
